b2233c0f0fd7a86402e9bb07ea012026fa2f6832
YassinOj <82060543+YassinOj@users.noreply.github.com>
2021-04-18 02:56:43 +0200

    Update test.py

--- TDD/Tests/test.py
+++ TDD/Tests/test.py
@@ -24,7 +24,7 @@ maison1 = Logement.maison(adresse, proprietaire, date_construction, surface, nb_
 
 
 
-class Test(unittest.TestCase):
+class TestMaison(unittest.TestCase):
 
     def test_getAdress(self):
         self.assertEqual(maison1.getAdress(), 'Somewhere in Earth', "Should be Somewhere in Earth")
@@ -36,6 +36,29 @@ class Test(unittest.TestCase):
         self.assertEqual(maison1.getSurface(), 120, 'Should be 120')
     def test_get_fenetres(self):
         self.assertEqual(maison1.getFenetres(), 20, 'Should be 20')
+        
+#immeuble 1
+adresse = 'Somewhere else in Earth'
+proprietaire = 'Someone else'
+date_construction = '01/01/1999'
+nb_appartement = 60
+ascenseur = True
+
+immeuble1 = Logement.immeuble(adresse, proprietaire, date_construction, nb_appartement, ascenseur)
+
+
+class TestImmeuble(unittest.TestCase):
+
+    def test_getAdress(self):
+        self.assertEqual(immeuble1.getAdress(), 'Somewhere else in Earth', "Should be 'Somewhere else in Earth'")
+
+    def test_getProp(self):
+        self.assertEqual(immeuble1.getProp(), 'Someone else', 'Should be Someone else')
+
+    def test_nbApp(self):
+        self.assertEqual(immeuble1.getNbApp(), 60, 'Should be 60')
+    def test_Ascenseur(self):
+        self.assertEqual(immeuble1.getAscn(), True, 'Should be True')
 
 if __name__ == '__main__':
     unittest.main()
