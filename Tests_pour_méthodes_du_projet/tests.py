2383ee37ca000ce220828ece210d1c083c3d4de4
YoussefBenCheikh <81634385+YoussefBenCheikh@users.noreply.github.com>
2021-05-30 20:17:28 +0200

    Update tests.py

--- Tests_pour_méthodes_du_projet/tests.py
+++ Tests_pour_méthodes_du_projet/tests.py
@@ -1,27 +1,28 @@
 import unittest
-import utils
+
+from utils import *
 from preprocess_data import PreprocessData
 from extract_data import ExtractData
 
-
+dic = {'paper_id': 'ABC00001', 'metadata': {'title': 'Covid', 'authors': []}, 'body_text': [{'text': 'Covid is coron virus', 'cite_spans': [], 'section': '', 'ref_spans': []}], 'ref_entries': {}, 'back_matter': [], 'bib_entries': {'BIBREF0': {'title': 'The possible macroeconomic effect on the UK of an influenza pandemic', 'authors': [], 'year': 2009, 'venue': '', 'volume': '', 'issn': '', 'pages': None, 'other_ids': {'DOI': []}}}}
+path_file = "/Users/youssefbencheikh/Desktop/ABC00001.json"
 class Test_utils(unittest.TestCase):
 
     def test_isnumber(self):
         self.assertEqual(is_number(12), True, "Should be True")
         self.assertEqual(is_number("Covid"), False, "Should be False")	
 
-    dic = {'paper_id': 'ABC00001', 'metadata': {'title': 'Covid', 'authors': []}, 'body_text': [{'text': 'Covid is coron virus', 'cite_spans': [], 'section': '', 'ref_spans': []}], 'ref_entries': {}, 'back_matter': [], 'bib_entries': {'BIBREF0': {'title': 'The possible macroeconomic effect on the UK of an influenza pandemic', 'authors': [], 'year': 2009, 'venue': '', 'volume': '', 'issn': '', 'pages': None, 'other_ids': {'DOI': []}}}}
     def test_read_json(self):
     	self.assertEqual(read_json(path_file), dic, "Should be a dictionnary")
 
-
+data = ExtractData(dic)
 class Test_extract_data(unittest.TestCase):
 	def test_get_paper_id(self):
-		self.assertEqual(dic.get_paper_id(), 'ABC00001', "Should be 'ABC00001'")
+		self.assertEqual(data.get_paper_id(), 'ABC00001', "Should be 'ABC00001'")
 	def test_get_title(self):
-		self.assertEqual(dic.get_title(), 'Covid', "Should be Covid")
-	def test_gat_text(self):
-		self.assertEqual(dic.get_text(), 'Covid is coron virus', "Should be Covid is coron virus")
+		self.assertEqual(data.get_title(), 'covid', "Should be Covid")
+	def test_get_text(self):
+		self.assertEqual(data.get_text(), 'Covid is coron virus', "Should be Covid is coron virus")
 
 
 preprocess = PreprocessData("test")
@@ -37,8 +38,11 @@ class Test_preprocess_data(unittest.TestCase):
 	def test_remove_stop_words(self):
 		self.assertEqual(preprocess.remove_stop_words("hello number 1 and 2"), 'hello number 1 2', "Should be 'hello number 1 2'")
 	def test_remove_special_character(self):
-		self.assertEqual(preprocess.remove_special_character("hello number @1 and #2"), 'hello number 1 2', "Should be 'hello number 1 2'")
+		self.assertEqual(preprocess.remove_special_character("hello number @1 and #2"), 'hello number 1 and 2', "Should be 'hello number 1 2'")
 	def test_lemmatization_text(self):
 		self.assertEqual(preprocess.lemmatization_text("oranges and apples"), 'orange and apple', "Should be 'orange and apple'")
+	
+if __name__ == '__main__':
+    unittest.main()
 	
 	
